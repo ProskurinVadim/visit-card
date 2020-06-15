e53de2d5f8b2b7c2fc34c0f857e0e3af2a75cf18
Sergey Savchenko <savchenkoserhii123@gmail.com>
2020-06-15 18:52:31 +0300

    Updates

---
+++ precache-manifest.342823b475a956dc5fad25992cd2e4ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63120e8099ea643887f4043485e70657",
+    "url": "/visit-card/index.html"
+  },
+  {
+    "revision": "8ace79ef3be418e647e4",
+    "url": "/visit-card/static/css/main.56a944b5.chunk.css"
+  },
+  {
+    "revision": "5bac6cd62b9cc26177b7",
+    "url": "/visit-card/static/js/2.e9beb46c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/visit-card/static/js/2.e9beb46c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ace79ef3be418e647e4",
+    "url": "/visit-card/static/js/main.3243d79d.chunk.js"
+  },
+  {
+    "revision": "6c25d8b0c545defd524e",
+    "url": "/visit-card/static/js/runtime-main.26adc049.js"
+  }
+]);
